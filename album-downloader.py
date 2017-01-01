f1e541d82ba67d52eabc0502adb92a8637ca9c29
kartik <kartikanand1992@gmail.com>
2017-01-01 22:02:46 +0530

    add progress bar

--- album-downloader.py
+++ album-downloader.py
@@ -4,6 +4,7 @@ import os
 import re
 import sys
 
+import progressbar
 import requests
 
 from imgurpython import ImgurClient
@@ -13,6 +14,18 @@ def get_extension(link):
     ext = regex.search(link).group()
 
     return ext
+
+
+def download_img(img):
+    file_ext = get_extension(img.link)
+    resp = requests.get(img.link, stream=True)
+
+    # create unique name by combining file id with its extension
+    file_name = img.id + file_ext
+
+    with open(file_name, 'wb') as f:
+        for chunk in resp.iter_content(chunk_size=1024):
+            f.write(chunk)
 
 
 try:
@@ -27,15 +40,8 @@ client = ImgurClient(client_id, client_secret)
 
 img_lst = client.get_album_images(album_id)
 
-for i in img_lst:
-    file_ext = get_extension(i.link)
-    resp = requests.get(i.link, stream=True)
+bar = progressbar.ProgressBar()
 
-    # create unique name by combining file id with its extension
-    file_name = i.id + file_ext
-
-    with open(file_name, 'wb') as f:
-        for chunk in resp.iter_content(chunk_size=1024):
-            f.write(chunk)
-
+for img in bar(img_lst):
+    download_img(img)
 
